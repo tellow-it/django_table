581068216009f49d2e5db171098f02558dbfd1a0
Ivan <mr.tikhonov1@gmail.com>
2022-04-15 14:36:22 +0300

    first commit

--- main/views.py
+++ main/views.py
@@ -7,5 +7,5 @@ from .models import Table
 class TableView(ListView):
     model = Table
     template_name = 'main/list_table.html'
-    paginate_by = 5
+    paginate_by = 7
     context_object_name = 'table_list'
